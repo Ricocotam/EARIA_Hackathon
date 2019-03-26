cae045cc842c6bf9e5f1307b72c4cc1aafc4e454
AntoineGourru <antoine.gourru@gmail.com>
2019-03-26 15:11:12 +0100

    simpler html parser

--- src/testBeautifulSoup.py
+++ src/testBeautifulSoup.py
@@ -2,39 +2,23 @@ import urllib.request
 from urllib.parse import urlparse
 import re
 
-page = "http://antoinegourru.com"
-# Recupération du contenu html de la page
+def getText(page):
+    # Recupération du contenu html de la page
 
-fp = urllib.request.urlopen(page)
-mybytes = fp.read()
+    fp = urllib.request.urlopen(page)
+    mybytes = fp.read()
+    
+    mystr = mybytes.decode("utf8")
+    fp.close()
 
-# On récupère le nom de la source
-source = urlparse(page).hostname
-#print(source)
+    # On parse le html
 
-mystr = mybytes.decode("utf8")
-fp.close()
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(mystr, 'html.parser')
 
-# On parse le html
+    txt = [re.sub(' +',' ',(''.join(node.findAll(text=True))).replace("\t", " ").replace("\r\n",".")) for node in soup.findAll('p')]
+    txt = ".".join([stri.replace("\n", "") for stri in txt])
+    return(txt)
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(mystr, 'html.parser')
-
-#On récupère ici le titre de la page
-
-#print(soup.title.string)
-
-# On extrait les lien vers d'autre pages
-
-x = [link.get('href') for link in soup.find_all('a')]
-regex = re.compile(r'http')
-selected_files = list(filter(regex.match, x))
-liens = [(source,urlparse(url).hostname) for url in selected_files]
-print(liens)
-
-# On récupère tout le contenu textuel
-
-txt = [(''.join(node.findAll(text=True))).replace("\t", " ") for node in soup.findAll('p')]
-#print(txt)
-
-
+txt = getText("http://antoinegourru.com")
+print(txt)
